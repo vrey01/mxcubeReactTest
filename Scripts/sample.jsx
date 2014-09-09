11fa1007bd1d0f8616e872758b87dc0fde8b3335
Vicente Rey Bakaikoa <vrey01@gmail.com>
2014-09-09 14:57:06 +0200

    renamed:    Scripts/motor.js -> Scripts/motor.jsx  	modified:   Scripts/sample.jsx  	renamed:    motreact.html -> motor_test.html  	modified:   samples_test.html  	modified:   samples_test.py

--- Scripts/sample.jsx
+++ Scripts/sample.jsx
@@ -1,14 +1,56 @@
 /** @jsx React.DOM */
 
+var SampleList = React.createClass({
+     render: function() {
+        id = 0;
+        var samples = this.props.samples.map( function(sample) {
+             id += 1;
+             return <Sample sample={sample} key={id} />;
+         }.bind(this));
+        return <div className="panel-group">{samples}</div>
+     },
+});
+
 var Sample = React.createClass({
      
-     getInitialState: function() {
-       return {}
-     },
-
      render: function() {
-       return <h1>Hello</h1>
+
+       var idref = "#"+this.props.key;
+
+       var fields = [];
+
+       fieldno = 0;
+       for (field in this.props.sample) { 
+           var value = this.props.sample[field];
+           fields.push( <EditableField key={fieldno} name={field} value={value} /> );
+           fieldno += 1;
+       }
+
+       return <div className="panel panel-default">
+    <div className="panel-heading">
+      <h4 className="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href={idref}>
+          {this.props.sample.sampleName}
+        </a>
+      </h4>
+    </div>
+    <div id={this.props.key} className="panel-collapse collapse out">
+      <div className="panel-body">
+            {fields}
+      </div>
+    </div>
+  </div>
      },
 
 });
 
+var EditableField = React.createClass({
+
+   componentDidMount: function() {
+    $(this.refs.editable.getDOMNode()).editable();
+   }, 
+
+   render: function() {
+       return <p>{this.props.name}: <a href="#" ref="editable" name={this.props.name} data-type="text" data-title="Edit value">{this.props.value}</a></p>
+   } 
+})
--- samples_test.py
+++ samples_test.py
@@ -18,6 +18,7 @@ Based on:
  
 # Bottle requires gevent.monkey.patch_all() even if you don't like it.
 import time, os,sys
+import json
 
 from gevent import monkey; monkey.patch_all()
 from gevent import sleep
@@ -25,47 +26,22 @@ from gevent import sleep
 from bottle import get, post, request, response, route, static_file
 from bottle import GeventServer, run
 
-samples_info="""{'status': {'code': 'ok'}, 'loaded_sample': [{'holderLength': 22.0, 'code': None, 'containerSampleChangerLocation': '1', 'proteinAcronym': 'Mnth', 'cellGamma': 0.0, 'cellAlpha': 0.0, 'sampleId': 444178, 'cellBeta': 0.0, 'crystalSpaceGroup': 'R32', 'sampleLocation': '1', 'sampleName': 'sample-E01', 'cellA': 0.0, 'diffractionPlan': (diffractionPlanWS3VO){
-diffractionPlanId = 266040
-experimentKind = "Default"
-exposureTime = 0.0
-}, 'cellC': 0.0, 'cellB': 0.0, 'experimentType': 'Default'}, {'holderLength': 22.0, 'code': None, 'containerSampleChangerLocation': '1', 'proteinAcronym': 'Mnth', 'cellGamma': 0.0, 'cellAlpha': 0.0, 'sampleId': 444179, 'cellBeta': 0.0, 'crystalSpaceGroup': 'R32', 'sampleLocation': '2', 'sampleName': 'sample-E02', 'cellA': 0.0, 'diffractionPlan': (diffractionPlanWS3VO){
-diffractionPlanId = 266041
-experimentKind = "Default"
-exposureTime = 0.0
-}, 'cellC': 0.0, 'cellB': 0.0, 'experimentType': 'Default'}, {'holderLength': 22.0, 'code': None, 'containerSampleChangerLocation': '1', 'proteinAcronym': 'Mnth', 'cellGamma': 0.0, 'cellAlpha': 0.0, 'sampleId': 444180, 'cellBeta': 0.0, 'crystalSpaceGroup': 'R32', 'sampleLocation': '3', 'sampleName': 'sample-E03', 'cellA': 0.0, 'diffractionPlan': (diffractionPlanWS3VO){
-diffractionPlanId = 266042
-experimentKind = "Default"
-exposureTime = 0.0
-}, 'cellC': 0.0, 'cellB': 0.0, 'experimentType': 'Default'}, {'holderLength': 22.0, 'code': None, 'containerSampleChangerLocation': '1', 'proteinAcronym': 'Mnth', 'cellGamma': 0.0, 'cellAlpha': 0.0, 'sampleId': 444181, 'cellBeta': 0.0, 'crystalSpaceGroup': 'R32', 'sampleLocation': '4', 'sampleName': 'sample-E04', 'cellA': 0.0, 'diffractionPlan': (diffractionPlanWS3VO){
-diffractionPlanId = 266043
-experimentKind = "Default"
-exposureTime = 0.0
-}, 'cellC': 0.0, 'cellB': 0.0, 'experimentType': 'Default'}, {'holderLength': 22.0, 'code': None, 'containerSampleChangerLocation': '1', 'proteinAcronym': 'Mnth', 'cellGamma': 0.0, 'cellAlpha': 0.0, 'sampleId': 444182, 'cellBeta': 0.0, 'crystalSpaceGroup': 'R32', 'sampleLocation': '5', 'sampleName': 'sample-E05', 'cellA': 0.0, 'diffractionPlan': (diffractionPlanWS3VO){
-diffractionPlanId = 266044
-experimentKind = "Default"
-exposureTime = 0.0
-}, 'cellC': 0.0, 'cellB': 0.0, 'experimentType': 'Default'}, {'holderLength': 22.0, 'code': None, 'containerSampleChangerLocation': '1', 'proteinAcronym': 'Mnth', 'cellGamma': 0.0, 'cellAlpha': 0.0, 'sampleId': 444183, 'cellBeta': 0.0, 'crystalSpaceGroup': 'R32', 'sampleLocation': '6', 'sampleName': 'sample-E06', 'cellA': 0.0, 'diffractionPlan': (diffractionPlanWS3VO){
-diffractionPlanId = 266045
-experimentKind = "Default"
-exposureTime = 0.0
-}, 'cellC': 0.0, 'cellB': 0.0, 'experimentType': 'Default'}, {'holderLength': 22.0, 'code': None, 'containerSampleChangerLocation': '1', 'proteinAcronym': 'Mnth', 'cellGamma': 0.0, 'cellAlpha': 0.0, 'sampleId': 444184, 'cellBeta': 0.0, 'crystalSpaceGroup': 'R32', 'sampleLocation': '7', 'sampleName': 'sample-E07', 'cellA': 0.0, 'diffractionPlan': (diffractionPlanWS3VO){
-diffractionPlanId = 266046
-experimentKind = "Default"
-exposureTime = 0.0
-}, 'cellC': 0.0, 'cellB': 0.0, 'experimentType': 'Default'}, {'holderLength': 22.0, 'code': None, 'containerSampleChangerLocation': '1', 'proteinAcronym': 'Mnth', 'cellGamma': 0.0, 'cellAlpha': 0.0, 'sampleId': 444185, 'cellBeta': 0.0, 'crystalSpaceGroup': 'R32', 'sampleLocation': '8', 'sampleName': 'sample-E08', 'cellA': 0.0, 'diffractionPlan': (diffractionPlanWS3VO){
-diffractionPlanId = 266047
-experimentKind = "Default"
-exposureTime = 0.0
-}, 'cellC': 0.0, 'cellB': 0.0, 'experimentType': 'Default'}, {'holderLength': 22.0, 'code': None, 'containerSampleChangerLocation': '1', 'proteinAcronym': 'Mnth', 'cellGamma': 0.0, 'cellAlpha': 0.0, 'sampleId': 444186, 'cellBeta': 0.0, 'crystalSpaceGroup': 'R32', 'sampleLocation': '9', 'sampleName': 'sample-E09', 'cellA': 0.0, 'diffractionPlan': (diffractionPlanWS3VO){
-diffractionPlanId = 266048
-experimentKind = "Default"
-exposureTime = 0.0
-}, 'cellC': 0.0, 'cellB': 0.0, 'experimentType': 'Default'}, {'holderLength': 22.0, 'code': None, 'containerSampleChangerLocation': '1', 'proteinAcronym': 'Mnth', 'cellGamma': 0.0, 'cellAlpha': 0.0, 'sampleId': 444187, 'cellBeta': 0.0, 'crystalSpaceGroup': 'R32', 'sampleLocation': '10', 'sampleName': 'sample-E10', 'cellA': 0.0, 'diffractionPlan': (diffractionPlanWS3VO){
-diffractionPlanId = 266049
-experimentKind = "Default"
-exposureTime = 0.0
+samples_info="""{'status': {'code': 'ok'}, 'loaded_sample': [ {'holderLength': 22.0, 'code': None, 'containerSampleChangerLocation': '1', 'proteinAcronym': 'Mnth', 'cellGamma': 0.0, 'cellAlpha': 0.0, 'sampleId': 444178, 'cellBeta': 0.0, 'crystalSpaceGroup': 'R32', 'sampleLocation': '1', 'sampleName': 'sample-E01', 'cellA': 0.0, 'diffractionPlan': {}, 'cellC': 0.0, 'cellB': 0.0, 'experimentType': 'Default'}, 
+{'holderLength': 22.0, 'code': None, 'containerSampleChangerLocation': '1', 'proteinAcronym': 'Mnth', 'cellGamma': 0.0, 'cellAlpha': 0.0, 'sampleId': 444179, 'cellBeta': 0.0, 'crystalSpaceGroup': 'R32', 'sampleLocation': '2', 'sampleName': 'sample-E02', 'cellA': 0.0, 'diffractionPlan': {}, 'cellC': 0.0, 'cellB': 0.0, 'experimentType': 'Default'}, {'holderLength': 22.0, 'code': None, 'containerSampleChangerLocation': '1', 'proteinAcronym': 'Mnth', 'cellGamma': 0.0, 'cellAlpha': 0.0, 'sampleId': 444180, 'cellBeta': 0.0, 'crystalSpaceGroup': 'R32', 'sampleLocation': '3', 'sampleName': 'sample-E03', 'cellA': 0.0, 'diffractionPlan': {}, 'cellC': 0.0, 'cellB': 0.0, 'experimentType': 'Default'}, {'holderLength': 22.0, 'code': None, 'containerSampleChangerLocation': '1', 'proteinAcronym': 'Mnth', 'cellGamma': 0.0, 'cellAlpha': 0.0, 'sampleId': 444181, 'cellBeta': 0.0, 'crystalSpaceGroup': 'R32', 'sampleLocation': '4', 'sampleName': 'sample-E04', 'cellA': 0.0, 'diffractionPlan': {
+}, 'cellC': 0.0, 'cellB': 0.0, 'experimentType': 'Default'}, {'holderLength': 22.0, 'code': None, 'containerSampleChangerLocation': '1', 'proteinAcronym': 'Mnth', 'cellGamma': 0.0, 'cellAlpha': 0.0, 'sampleId': 444182, 'cellBeta': 0.0, 'crystalSpaceGroup': 'R32', 'sampleLocation': '5', 'sampleName': 'sample-E05', 'cellA': 0.0, 'diffractionPlan': {
+'diffractionPlanId' : 266042,
+'experimentKind' : "Default",
+'exposureTime' : 0.0,
+}, 'cellC': 0.0, 'cellB': 0.0, 'experimentType': 'Default'}, {'holderLength': 22.0, 'code': None, 'containerSampleChangerLocation': '1', 'proteinAcronym': 'Mnth', 'cellGamma': 0.0, 'cellAlpha': 0.0, 'sampleId': 444183, 'cellBeta': 0.0, 'crystalSpaceGroup': 'R32', 'sampleLocation': '6', 'sampleName': 'sample-E06', 'cellA': 0.0, 'diffractionPlan': {
+}, 'cellC': 0.0, 'cellB': 0.0, 'experimentType': 'Default'}, {'holderLength': 22.0, 'code': None, 'containerSampleChangerLocation': '1', 'proteinAcronym': 'Mnth', 'cellGamma': 0.0, 'cellAlpha': 0.0, 'sampleId': 444184, 'cellBeta': 0.0, 'crystalSpaceGroup': 'R32', 'sampleLocation': '7', 'sampleName': 'sample-E07', 'cellA': 0.0, 'diffractionPlan': {
+}, 'cellC': 0.0, 'cellB': 0.0, 'experimentType': 'Default'}, {'holderLength': 22.0, 'code': None, 'containerSampleChangerLocation': '1', 'proteinAcronym': 'Mnth', 'cellGamma': 0.0, 'cellAlpha': 0.0, 'sampleId': 444185, 'cellBeta': 0.0, 'crystalSpaceGroup': 'R32', 'sampleLocation': '8', 'sampleName': 'sample-E08', 'cellA': 0.0, 'diffractionPlan': {
+}, 'cellC': 0.0, 'cellB': 0.0, 'experimentType': 'Default'}, {'holderLength': 22.0, 'code': None, 'containerSampleChangerLocation': '1', 'proteinAcronym': 'Mnth', 'cellGamma': 0.0, 'cellAlpha': 0.0, 'sampleId': 444186, 'cellBeta': 0.0, 'crystalSpaceGroup': 'R32', 'sampleLocation': '9', 'sampleName': 'sample-E09', 'cellA': 0.0, 'diffractionPlan': {
+}, 'cellC': 0.0, 'cellB': 0.0, 'experimentType': 'Default'}, {'holderLength': 22.0, 'code': None, 'containerSampleChangerLocation': '1', 'proteinAcronym': 'Mnth', 'cellGamma': 0.0, 'cellAlpha': 0.0, 'sampleId': 444187, 'cellBeta': 0.0, 'crystalSpaceGroup': 'R32', 'sampleLocation': '10', 'sampleName': 'sample-E10', 'cellA': 0.0, 'diffractionPlan': {
 }, 'cellC': 0.0, 'cellB': 0.0, 'experimentType': 'Default'}]}"""
+
+@get('/samples')
+def return_samples():
+    return json.dumps(eval(samples_info))
 
 @route("/")
 def main():
@@ -74,10 +50,6 @@ def main():
 @route("/<url:path>")
 def serve_static_file(url):
    return static_file(url, os.path.dirname(__file__))
-
-@post('/samples')
-def return_samples():
-    return samples_info
 
 if __name__ == '__main__':
     run(host="", port="8080",server=GeventServer)
